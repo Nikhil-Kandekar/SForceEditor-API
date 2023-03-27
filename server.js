72ff4670af26000c3e6e0f27fd07451fb4582248
Nikhil-Kandekar <nikhilbkandekar@gmail.com>
2023-03-27 11:55:37 +0530

    Basic API created

---
+++ server.js
@@ -0,0 +1,9 @@
+const app=require("express");
+const PORT = process.env.port || 3000;
+app.get("",(req,res)=>{
+    res.send("This is SForceEditor API");
+})
+
+app.listen(PORT,()=>{
+    console.log(`App is running on port ${PORT}`);
+})
